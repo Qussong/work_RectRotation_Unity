ca501aaf700a4b3e9256e156a97b012ef9fc8d68
Gihoon <89381041+Qussong@users.noreply.github.com>
2025-02-05 02:26:44 +0900

    Rectange Rotation 회전방향 변경 기능 추가
    문제점 : 바닥에 도달한 후 각도 보정하는 과정에서 문제발생 -> 보정 기능 제거

--- Assets/Gihoon/Scripts/RectRottation.cs
+++ Assets/Gihoon/Scripts/RectRottation.cs
@@ -14,7 +14,7 @@ public class RectRottation : MonoBehaviour
         RB  // RightBottom (3)
     }
 
-    [Header("Property")]
+    [Header("Essential Settings")]
     [SerializeField] Image rectImage = null;
     [SerializeField] Image leftBottomCorner = null;
     [SerializeField] Image leftTopCorner = null;
@@ -22,15 +22,19 @@ public class RectRottation : MonoBehaviour
     [SerializeField] Image rightBottomCorner = null;
     [SerializeField][Tooltip("사이클로이드 곡선을 긋는 선")] Image cycloidPoint = null;
     [SerializeField][Tooltip("회전 중심 점")] Image pivotPoint = null;
-    [SerializeField][Tooltip("회전 판별 점")] Image rightPoint = null;
+    [SerializeField][Tooltip("바닥 충돌 점")] Image touchPoint = null;
 
-    [Header("Status")]
+    [Header("Properties")]
+    [SerializeField][Tooltip("초당 회전 각도 (speed)")] float rotationAnglePerSecond = 0f;
+    [SerializeField][Tooltip("회전 방향 ( true : right(→) , false : left(←) )")] bool rotationDirection = true;
+    bool prevRotationDirection = true;
+
+    [Header("Statuses")]
     [SerializeField][Tooltip("시작 지점")] Vector3 StartPoint = Vector3.zero;
     [SerializeField][Tooltip("현재 회전량")] float curRotation = 0f;
-    [SerializeField][Tooltip("초당 회전 각도")] float rotationAngle = 0f;
     [SerializeField] int cycloidPos = -1;
     [SerializeField] int pivotPos = -1;
-    [SerializeField] int rightPos = -1;
+    [SerializeField] int touchPos = -1;
 
     Image[] corners = new Image[4];
 
@@ -51,31 +55,73 @@ public class RectRottation : MonoBehaviour
 
         cycloidPos = (int)CornerName.LB;
         pivotPos = (int)CornerName.RB;
-        rightPos = (int)CornerName.RT;
+        touchPos = (int)CornerName.RT;
 
         cycloidPoint.rectTransform.localPosition = corners[cycloidPos].rectTransform.localPosition;
         pivotPoint.rectTransform.localPosition = corners[pivotPos].rectTransform.localPosition;
-        rightPoint.rectTransform.localPosition = corners[rightPos].rectTransform.localPosition;
+        touchPoint.rectTransform.localPosition = corners[touchPos].rectTransform.localPosition;
 
-        rotationAngle = 10;
+        rotationAnglePerSecond = 50;
     }
 
     void Update()
     {
-        rectImage.rectTransform.RotateAround(pivotPoint.rectTransform.position, Vector3.back, rotationAngle * Time.deltaTime);
-   
-        curRotation = rectImage.rectTransform.rotation.eulerAngles.z;
-        if (rightPoint.transform.position.y <= pivotPoint.transform.position.y)
+        // When rotation direction change
+        if(prevRotationDirection != rotationDirection)
         {
-            rectImage.rectTransform.rotation = Quaternion.Euler(0, 0, InterpolationAngle(curRotation));
-
-            pivotPos = ModuloOperator(pivotPos);
-            rightPos = ModuloOperator(rightPos);
-            pivotPoint.rectTransform.localPosition = corners[pivotPos].rectTransform.localPosition;
-            rightPoint.rectTransform.localPosition = corners[rightPos].rectTransform.localPosition;
-
-            //Time.timeScale = 0;
+            // When new rotation direction is RIGHT
+            if(rotationDirection == true)
+            {
+                // Touch Point Position = pivot - 1
+                touchPos = ModuloOperatorRight(pivotPos);
+            }
+            // When new rotation direction is LEFT
+            else
+            {
+                // Touch Point Position = pivot + 1
+                touchPos = ModuloOperatorLeft(pivotPos);
+            }
+            touchPoint.rectTransform.localPosition = corners[touchPos].rectTransform.localPosition;
         }
+
+
+        // Rotation Direction : right
+        if (rotationDirection)
+        {
+            rectImage.rectTransform.RotateAround(pivotPoint.rectTransform.position, Vector3.back, Mathf.Abs(rotationAnglePerSecond) * Time.deltaTime);
+
+            // When the TouchPoint touch ground
+            if (touchPoint.transform.position.y < pivotPoint.transform.position.y)
+            {
+                //rectImage.rectTransform.localRotation = Quaternion.Euler(0, 0, InterpolationAngle(curRotation));
+                //rectImage.rectTransform.rotation = Quaternion.Euler(0, 0, InterpolationAngle(curRotation));
+                //Time.timeScale = 0;
+
+                pivotPos = ModuloOperatorRight(pivotPos);
+                touchPos = ModuloOperatorRight(touchPos);
+                pivotPoint.rectTransform.localPosition = corners[pivotPos].rectTransform.localPosition;
+                touchPoint.rectTransform.localPosition = corners[touchPos].rectTransform.localPosition;
+            }
+        }
+        // Rotation Direction : left
+        else
+        {
+            rectImage.rectTransform.RotateAround(pivotPoint.rectTransform.position, Vector3.forward, Mathf.Abs(rotationAnglePerSecond) * Time.deltaTime);
+
+            // When the TouchPoint touch ground
+            if (touchPoint.transform.position.y <= pivotPoint.transform.position.y)
+            {
+                //rectImage.rectTransform.rotation = Quaternion.Euler(0, 0, InterpolationAngle(curRotation));
+
+                pivotPos = ModuloOperatorLeft(pivotPos);
+                touchPos = ModuloOperatorLeft(touchPos);
+                pivotPoint.rectTransform.localPosition = corners[pivotPos].rectTransform.localPosition;
+                touchPoint.rectTransform.localPosition = corners[touchPos].rectTransform.localPosition;
+            }
+        }
+
+        curRotation = rectImage.rectTransform.rotation.eulerAngles.z;
+        prevRotationDirection = rotationDirection;
     }
 
     float InterpolationAngle(float angle)
@@ -84,15 +130,22 @@ public class RectRottation : MonoBehaviour
 
         if (angle <= 90) result = 90f;
         else if (angle <= 180) result = 180f;
-        else if (angle <= 2700) result = 270f;
+        else if (angle <= 270) result = 270f;
         else result = 360f;
 
         return result;
     }
 
-    int ModuloOperator(int curValue)
+    int ModuloOperatorRight(int curValue)
     {
         int nextValue = (curValue - 1 + 4) % 4;
         return nextValue;
     }
+
+    int ModuloOperatorLeft(int curValue)
+    {
+        int nextValue = (curValue + 1) % 4;
+        return nextValue;
+    }
+
 }
